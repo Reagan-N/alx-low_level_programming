06334d6d088922a41c4e48f94e429b1b7f3d1634
Reagan-N <nkongeraeagan@gmail.com>
2022-09-12 15:01:20 -0700

    Finally finished

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - (and that piece of art is useful" - Dora Korpar, 2015-10-19)
- * return: and that piece of art is useful" - Dora Korpar, 2015-10-19 + 1
+ * main - (and that piece of art is useful)
+ * Return: and that piece of art is useful + 1
  */
 
 int main(void)
