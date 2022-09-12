e2fd39675bf189605f266cb713381ca7f5ee3f52
Reagan-N <nkongeraeagan@gmail.com>
2022-09-12 12:28:13 -0700

    Almost there

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,5 @@
 int main(void)
 {
 	printf("Size of a char: %s\nSize of an int: %s\nSize of a long int: %s\nSize of a long long int: %s\nSize of a float: %s\n", "1 byte(s)", "4 byte(s)", "4 byte(s)", "8 byte(s)", "4 byte(s)");
-	printf("Size of a char: %s\nSize of an int: %s\nSize of a long int: %s\nSize of a long long int: %s\nSize of a float: %s\n", "1 byte(s)", "4 byte(s)", "8 byte(s)", "8 byte(s)", "4 byte(s)");
 	return (0);
 }
