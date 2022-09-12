350fcfc552278c1010193a5e986c3f2447691094
Reagan-N <nkongeraeagan@gmail.com>
2022-09-12 12:10:27 -0700

    Almost there

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - (prints the size of various types on the computer it is compiled and run on)
+* Return: prints the size of various types on the computer it is compiled and run on + 0
+*/
+
+int main(void)
+{
+	printf("Size of a char: %s\nSize of an int: %s\nSize of a long int: %s\nSize of a long long int: %s\nSize of a float: %s\n", "1 byte(s)", "4 byte(s)", "8 byte(s)", "8 byte(s)", "4 byte(s)");
+	return (0);
+}
