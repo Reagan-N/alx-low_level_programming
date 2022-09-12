b190f651be6ac38131c5d534ca9c2619cb3d6d68
Reagan-N <nkongeraeagan@gmail.com>
2022-09-12 09:45:12 -0700

    Am getting there

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+/**
+ * puts - is used to write a line or string to the output( stdout ) stream
+ */
+
 int main(void)
 {
-	char *str = "Programming is like building a multilingual puzzle";
+	char *str = ""Programming is like building a multilingual puzzle";
 
 	puts(str);
 
