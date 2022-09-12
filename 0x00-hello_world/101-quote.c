e9f133296e3b798dcd82858ae8f8197043422ee3
Reagan-N <nkongeraeagan@gmail.com>
2022-09-12 13:09:32 -0700

    Finally

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - (and that piece of art is useful" - Dora Korpar, 2015-10-19)
+ * return: and that piece of art is useful" - Dora Korpar, 2015-10-19 + 1
+ */
+
+int main(void)
+{
+	printf("%s\n", "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
