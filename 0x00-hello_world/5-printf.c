a2d150671ba6f877a6d562801b0afad263f72a85
Reagan-N <nkongeraeagan@gmail.com>
2022-09-12 10:58:35 -0700

    I got this

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* print - with proper grammar, but the outcome is a piece of art,
+* main - (with proper grammar, but the outcome is a piece of art,)
 * Return: with proper grammar, but the outcome is a piece of art, + 0
 */
 
