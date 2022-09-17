f29afc5bfaf997ef29dd32d81aa97424213a81ce
Reagan-N <nkongeraeagan@gmail.com>
2022-09-17 15:59:31 -0700

    All the way

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Alphabets
+ *Return: None
+ */
+
+int main(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		putchar(alp);
+		alp++;
+	}
+	putchar('\n');
+		return (0);
+}
