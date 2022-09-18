61c9d7132e278eef3ee3423b9043a714d1157a29
Reagan-N <nkongeraeagan@gmail.com>
2022-09-18 07:28:57 -0700

    All the way

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - numbers
+ *Return: none
+ */
+
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
