f07615d41b990e2b90f378dbee361ed798788771
Reagan-N <nkongeraeagan@gmail.com>
2022-09-18 09:21:19 -0700

    All the way

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+/**
+ *main - inventing
+ *Return: none
+ */
+
+int main(void)
+{
+	int t = '0', r = '0';
+
+	while (t <= 9)
+	{
+	r = 0;
+	while (r <= 9)
+	{
+	if (t != r && t < r)
+	{
+		putchar(t);
+		putchar(r);
+
+		if (!(t == 8 && r == 9))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	r++;
+	}
+	t++;
+	}
+	putchar('\n');
+	return (0);
+}
