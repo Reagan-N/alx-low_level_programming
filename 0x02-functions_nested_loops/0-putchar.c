c1aec5cdbd61b89f081e73a9e3e03d47042af2df
Reagan-N <nkongeraeagan@gmail.com>
2022-09-22 15:54:02 -0700

    All that mattera

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: zero
+ */
+int _putchar(char c)
+{
+	char i;
+	i = "_putchar";
+
+	_putchar(i);
+	_putchar("\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef MY_HEADER_FILE_H
+#define MY_HEADER_FILE_H
+
+int _putchar(char c);
+void print_alphabet(void);
+
+#endif
